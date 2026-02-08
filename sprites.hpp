f255ed1d70e7e7fa03daea11e09b8d1e834ff018
KedengKedeng <tychohartingsveld@gmail.com>
2026-02-08 21:54:36 +0100

    split enums from repository files

---
+++ sprites.hpp
@@ -0,0 +1,85 @@
+#pragma once
+
+enum class Sprites {
+	Spoon,
+
+	BlueSlime,
+	GreenSlime,
+
+	SlipperyOrb,
+	SlimeTape,
+	RainBoots,
+	GliderWings,
+	StoneWheel,
+	RocketBooster,
+	StickyOrb,
+	BurnCream,
+	GrapplingHook,
+	ClimbingGloves,
+	StickyBomb,
+	StormInABottle,
+	WindCharge,
+	ShipInABottle,
+	LightningGem,
+	IceScates,
+	MuddyBlob,
+	ObsidianGem,
+	ClayBall,
+	MagmaBall,
+	FireCharge,
+
+	EmptyCauldron,
+	CauldronCloseupFront,
+	CauldronCloseupBack
+};
+
+enum class SpriteSheets {
+	PlayerIdleLeft,
+	PlayerIdleRight,
+	PlayerWalkLeft,
+	PlayerWalkRight,
+
+	PenguinIdleLeft,
+	PenguinIdleRight,
+	PenguinWalkLeft,
+	PenguinWalkRight,
+
+	BlueSlimeIdleLeft,
+	BlueSlimeIdleRight,
+	BlueSlimeWalkLeft,
+	BlueSlimeWalkRight,
+	GreenSlimeIdleLeft,
+	GreenSlimeIdleRight,
+	GreenSlimeWalkLeft,
+	GreenSlimeWalkRight,
+	IceDragonIdleLeft,
+	IceDragonIdleRight,
+	IceDragonWalkLeft,
+	IceDragonWalkRight,
+	WindDragonIdleLeft,
+	WindDragonIdleRight,
+	WindDragonWalkLeft,
+	WindDragonWalkRight,
+	EarthDragonIdleLeft,
+	EarthDragonIdleRight,
+	EarthDragonWalkLeft,
+	EarthDragonWalkRight,
+	FireDragonIdleLeft,
+	FireDragonIdleRight,
+	FireDragonWalkLeft,
+	FireDragonWalkRight,
+
+	IceGem,
+	WindGem,
+	EarthGem,
+	FireGem,
+
+	EmptyBurningCauldron,
+	FilledCauldron,
+	FilledBurningCauldron,
+	CauldronCloseupFire,
+	CauldronCloseupFilled,
+
+	Blower,
+	Floor
+};
